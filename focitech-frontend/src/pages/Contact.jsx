e9230cffa1a3e3f69bf5dd0cf3339b4381ab035a
DeepeshSharma11 <deepeshtech8433@gmail.com>
2026-01-25 21:53:24 +0530

    change transmit to send enquiry

--- focitech-frontend/src/pages/Contact.jsx
+++ focitech-frontend/src/pages/Contact.jsx
@@ -209,7 +209,7 @@ const Contact = () => {
                       {status.loading ? (
                         <div className="w-6 h-6 border-2 border-white/30 border-t-white rounded-full animate-spin"></div>
                       ) : (
-                        <>Send Inquirie <Send size={18} /></>
+                        <>Send Enquiry <Send size={18} /></>
                       )}
                     </motion.button>
                   </form>
