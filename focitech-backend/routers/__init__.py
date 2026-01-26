ad641dd07db474a35484785a1fe02cbafbcc1aab
DeepeshSharma11 <deepeshtech8433@gmail.com>
2026-01-27 00:36:51 +0530

    update __init__.py

--- focitech-backend/routers/__init__.py
+++ focitech-backend/routers/__init__.py
@@ -2,3 +2,4 @@ from .projects import router as projects
 from .inquiries import router as inquiries
 from .team import router as team
 from .auth import router as auth 
+from .Careers import router as careers
